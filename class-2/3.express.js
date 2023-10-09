f2919f95535c0829b74713ac55e15cd07034b89e
¨Samuel <samiriverame07@gmail.com>
2023-10-09 16:56:54 -0600

    a

--- class-2/3.express.js
+++ class-2/3.express.js
@@ -6,22 +6,38 @@ const PORT = process.env.PORT ?? 1234
 const app = express()
 app.disable('x-powered-by')
 
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // Con los paremetros de arriba definidos a continuacion solo llegaran las req
+//   // que son POST y que tienen el header Content-type: aplication/json
+//   let body = ''
+
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     req.body = data
+//     next()
+//   })
+// })
+
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
 })
 
 app.post('/pokemon2', (req, res) => {
-  let body = ''
+  res.status(201).json(req.body)
+})
 
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.timestamp = Date.now()
-    res.status(201).json(data)
-  })
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
 })
 
 app.listen(PORT, () => {
