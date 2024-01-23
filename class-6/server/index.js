fe07adb9ef2b0bd1f156116eb0eddb100f59e776
Samuel Rivera <samiriverame07@gmail.com>
2024-01-23 01:29:28 -0600

    Conected the client whit the server and styling the client

--- class-6/server/index.js
+++ class-6/server/index.js
@@ -1,14 +1,30 @@
 import express from 'express'
 import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import {createServer} from 'node:http'
+
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+
+    socket.on('disconnect',() =>{
+        console.log('A user a disconnected');
+    })
+})
+
 app.use(logger('dev'))
 
 app.get('/',(req, res) =>{
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
-    console.log(`Server runnung on port  localhost:${port}`);
+server.listen(port, () => {
+    console.log(`Server runnung on port  http://localhost:${port}`);
 })
